30a6f55b4cb528a8fdf0abf6351a99fdd06d5582
marekkoc <m2019k1020@gmail.com>
2024-11-14 09:14:34 +0100

    update

--- streamlit/texh-with-tim/website-in-15-minutes/main.py
+++ streamlit/texh-with-tim/website-in-15-minutes/main.py
@@ -8,7 +8,7 @@ link: https://www.youtube.com/watch?v=2siBrMsqF44
 
 
 Created: 2024.11.11
-Modyfied: 2024.11.12
+Modyfied: 2024.11.13
 """
 
 import streamlit as st
@@ -21,7 +21,14 @@ uploaded_file = st.file_uploader("Choose CSV file", type='csv',)
 if uploaded_file: 
     st.write("File uploaded...")
 
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(uploaded_file, index_col=0, decimal='.')    
+    df['rok'] = df['rok'].astype(str)
+    #df['rok'] = df['rok'].astype(int)
+
+    for c in df.columns[1:]: 
+        df.style.format({ c: '{:.2f}'})
+    #df.style.format({"rok":'i'})
+   
 
     st.subheader("Data Preview")
     st.write(df.head())
@@ -30,5 +37,23 @@ if uploaded_file:
     st.write(df.describe())
 
     st.subheader("Filter Data")
-    columns = df.columns.to_list()
-    selected_column = st.selectbox("Select the monty to filter by...", columns)
+    year = df['rok'].unique()
+    selected_column = st.selectbox("Select a year...", year)
+
+    df_year = df[df['rok'] == selected_column]
+    st.write(df_year)
+
+    select_month = st.selectbox("Select a month...", df.columns[1:])
+    
+    st.subheader("Plot selected month weight in the year")
+
+    x_axis = list(range(1, 31))
+        
+    if st.button("Generate plot"):
+        st.line_chart(df_year[select_month]) 
+    
+else:
+    st.write("Waiting on file upload...")
+
+
+    
