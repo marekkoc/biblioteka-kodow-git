7174576f19c944762d0a6410bdca774df2c477a5
marekkoc <m2019k1020@gmail.com>
2024-11-14 09:15:30 +0100

    update

---
+++ udemy/Loek-van-den-Ouveland-OOP-with-Python-for-beginners/19-schedule-report/employee.py
@@ -0,0 +1,21 @@
+
+class Employee:
+    def __init__(self, first_name, last_name, salary):
+        self._first_name = first_name
+        self._last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+class Mechanic(Employee):
+    job_title = "Mechanic"  
+
+class Attendant(Employee):
+    job_title = "Station Attendant"
+
+class Cook(Employee):
+    job_title = "Cook"
+
+class Manager(Employee):
+    job_title = "Manager"
