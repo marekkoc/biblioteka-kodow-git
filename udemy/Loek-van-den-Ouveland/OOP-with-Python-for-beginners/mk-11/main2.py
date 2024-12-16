021f54530edb2f810e83de7c7d06830b126e1d67
marekkoc <m2019k1020@gmail.com>
2024-12-16 07:15:49 +0100

    update

---
+++ udemy/Loek-van-den-Ouveland/OOP-with-Python-for-beginners/mk-11/main2.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self, name:str, salary:int):
+        self.name: str = name
+        self.salary: int = salary
+
+
+employees: list[Employee] = [
+    Employee("Vera", 2000),
+    Employee("Chuck", 1800),
+    Employee("Samantha", 1800),
+    Employee("Roberto", 2100),
+    Employee("Dave", 2200),
+    Employee("Tina", 2300),
+    Employee("Ringo", 1900),
+]
+
+for e in employees:
+    if e.name == "Vera":
+        print(f"{e.name}, ${e.salary}, Manager")
+    elif e.name == "Chuck" or e.name == "Samantha":
+        print(f"{e.name}, ${e.salary}, Attendant")
+    elif e.name == "Roberto":
+        print(f"{e.name}, ${e.salary}, Cook")
+    elif e.name == "Dave" or e.name == "Tina" or e.name == "Ringo":
+        print(f"{e.name}, ${e.salary}, Car Repair")
