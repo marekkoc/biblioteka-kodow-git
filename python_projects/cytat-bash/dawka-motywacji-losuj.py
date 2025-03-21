d140ed80f62a3c99aa065c3bc30c4b69a1823008
marekkoc <m2019k1020@gmail.com>
2025-03-21 08:29:22 +0100

    2025.03.21

--- python_projects/cytat-bash/dawka-motywacji-losuj.py
+++ python_projects/cytat-bash/dawka-motywacji-losuj.py
@@ -3,7 +3,7 @@
 Author: @marekkoc
 
 Created: 2020.12.15 
-Updated: 2025.03.19
+Updated: 2025.03.20
 """
 
 import os
@@ -19,13 +19,24 @@ from mkquotes import (
                     JsonLoader
                     )
 
+# 2025.03.20
+quote_selector = QuoteSelector()
+quote_selector.set_json_loader(JsonLoader(FilePaths("dawka-motywacji", create="json")))
+quote = quote_selector.random_quote()
+print(f"{quote.tekst}\n  ** {quote.autor} **")
 
+# 2025.03.19
+# file_paths = FilePaths("dawka-motywacji")
+# json_loader = JsonLoader(file_paths)
+# quote_selector = QuoteSelector()
+# quote_selector.set_json_loader(json_loader)
+# quote = quote_selector.random_quote()
+# print(f"{quote.tekst}\n  ** {quote.autor} **")
 
-# 2021.10.19
-#hashseed = os.getenv('PYTHONHASHSEED')
-#if not hashseed:
-#    os.environ['PYTHONHASHSEED'] = '0'
-#    os.execv(sys.executable, [sys.executable] + sys.argv)
+# 2025.03.17
+#quote_selector = QuoteSelector(FilePaths("dawka-motywacji"))
+#quote = quote_selector.random_quote()
+#print(f"{quote.tekst}\n  ** {quote.autor} **")
 
 # 2025.03.18    
 #hashseed = os.getenv('PYTHONHASHSEED')
@@ -34,15 +45,8 @@ from mkquotes import (
 #    os.environ.pop('PYTHONHASHSEED', None)
 #    os.execv(sys.executable, [sys.executable] + sys.argv)    
 
-# 2025.03.17
-#quote_selector = QuoteSelector(FilePaths("dawka-motywacji"))
-#quote = quote_selector.random_quote()
-#print(f"{quote.tekst}\n  ** {quote.autor} **")
-
-# 2025.03.19
-file_paths = FilePaths("dawka-motywacji")
-json_loader = JsonLoader(file_paths)
-quote_selector = QuoteSelector()
-quote_selector.set_json_loader(json_loader)
-quote = quote_selector.random_quote()
-print(f"{quote.tekst}\n  ** {quote.autor} **")
+# 2021.10.19
+#hashseed = os.getenv('PYTHONHASHSEED')
+#if not hashseed:
+#    os.environ['PYTHONHASHSEED'] = '0'
+#    os.execv(sys.executable, [sys.executable] + sys.argv)
