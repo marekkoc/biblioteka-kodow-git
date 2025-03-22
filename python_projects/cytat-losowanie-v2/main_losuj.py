6c3026098c891ab0425c592840a9266dbe308bbc
marekkoc <m2019k1020@gmail.com>
2025-03-22 07:42:12 +0100

    2025.03.22

--- python_projects/cytat-losowanie-v2/main_losuj.py
+++ python_projects/cytat-losowanie-v2/main_losuj.py
@@ -13,34 +13,47 @@ from MainWindow import Ui_MainWindow
 
 
 from mkquotes import FilePaths
+from mkquotes import JsonLoader
 from mkquotes import Odt2TxtConverter
 from mkquotes import Txt2JsonConverter
 from mkquotes import QuoteSelector
 
+
+class MainWindow():
+    def __init__(self):
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(MainWindow)
+
+        self._create_selector()
+        self.ui.button_random.clicked.connect(self.losuj_cytat)
+
+        MainWindow.show()
+        sys.exit(app.exec_())
+
+    def _create_selector(self):
+        file_paths = FilePaths("dawka-motywacji", create="json")
+        self.quote_selector = QuoteSelector()
+        self.quote_selector.set_json_loader(JsonLoader(file_paths))
+        
+
+    def losuj_cytat(self):           
+        cytat = self.quote_selector.random_quote()
+        self.cytat_id = cytat.id
+        self.ui.label_cytat.setText(f"{cytat.tekst} ({cytat.id})")
+        self.ui.label_autor.setText(cytat.autor)
+
+
+
+    
+
+
+
 if __name__ == "__main__":
    
-    def losuj_cytat():
-        quote_selector = QuoteSelector(FilePaths("dawka-motywacji"))
-        cytat = quote_selector.random_quote()
-        ui.label_cytat.setText(cytat.tekst)
-        ui.label_autor.setText(cytat.autor)
 
-    app = QtWidgets.QApplication(sys.argv)
-    app.setStyle("Fusion")  # Lub inny styl: "Windows", "WindowsVista", itp.
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-
-    ui.button_random.clicked.connect(losuj_cytat)
+    #ui.button_random.clicked.connect(losuj_cytat)
 
 
-
-    MainWindow.show()
-    sys.exit(app.exec_())
-
- 
-    print()
-    quote_selector = QuoteSelector(FilePaths("dawka-motywacji"))
-    print(quote_selector.random_quote())
-    print()
-
+    MainWindow()
