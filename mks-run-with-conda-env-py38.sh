3e1a7e275ef33357f73b40c3d7743698cfea0b2b
marekkoc <m2019k1020@gmail.com>
2025-03-27 09:59:49 +0100

    update

--- mks-run-with-conda-env-py38.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# C: 2024.10.15
-# M: 2025.02.05
-#
-# Usage:
-# skrypt przyjmuje jeden argument: nazwa skryptu pythona do wykonania wraz z rozszerzeniem .py
-conda run -n py312 python $1 
-
